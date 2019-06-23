b034ab9417ab94be681cccaaec96cb389389ae69
Nikolaus Luhrs <nikolaus.luhrs@gmail.com>
2019-06-23 11:15:58 -0600

    converted to only non-blocking assignments

--- i2s_mic_core.v
+++ i2s_mic_core.v
@@ -12,28 +12,30 @@ reg drdyInternal;
 reg [5:0] clkreg;
 reg [17:0] dreg;
 assign lrclk=clkreg[5];
-always @(posedge drdyInternal)
-  if (convert2s) begin
-    if dreg[17] begin
-      dout=131072-(~dreg[16:0]+1);
-    end
-    else begin
-      dout=131027+dreg;
-    end
-  drdy=1;
-  end
-  
-always @(negedge dclk) begin
+
+always @(negedge dclk) begin 
   clkreg<=clkreg+1;
-end 
-always @(clkreg) begin
-  if (clkreg > 0 && clkreg <19) begin 
-    drdyInternal<=0;
+  if (&clkreg) begin
     drdy<=0;
+  if (clkreg >= 0 && clkreg <18) begin 
+    drdyInternal<=0;
     dreg[clkreg[4:0]-1]<=din;
   end
-  else begin
-    drdyInternal<=1;
+  if (clkreg ==18) begin // on clkreg == 19 convert from 2s compliment to unsigned+offset of 0x20000
+    if (convert2s) begin
+      if dreg[17] begin
+        dout<=131072-(~dreg[16:0]+1);
+      end
+      else begin
+        dout<=131027+dreg;
+      end
+    end
+    else begin
+      dout<=dreg;
+    end
+  end
+  if (clkreg == 19) begin
+    drdy<=1;
   end
 end
 
